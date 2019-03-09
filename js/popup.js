b03577e4d745af4ab2f0cd2c363dfbaa7d84328f
SoundM <galina45192@gmail.com>
2019-03-09 13:11:44 +0100

    Открыт попап при отключенном джс и скрыт при включенном

--- js/popup.js
+++ js/popup.js
@@ -11,6 +11,8 @@
 		var isStorageSupport = true; /* Некоторые браузеры не имеют поддержку localStorage, или он может быть отключен, нам стоит учитывать такую возможность. */
 		var storageAdults = "";
 		var storageChildren = "";
+		
+		popup.classList.remove("modal-noscript");
   
 		try {
 			storageAdults = localStorage.getItem("adults");
@@ -19,6 +21,7 @@
 			isStorageSupport = false;
 		}
 		
+		
 		link.addEventListener("click", function (evt) { /* Поймаем событие клика по этой кнопке. */
 			evt.preventDefault(); /*Отменим стандартное действие ссылки при нажатии на неё. */
 			popup.classList.toggle("modal-show"); /* С помощью метода classList.toggle добавляем этот класс к модальному окну по клику на ссылку, а при повторном клике убираем класс. т.е. он поочередно то добавляется, то исчезает */
@@ -26,13 +29,17 @@
 			if (!popup.classList.contains("modal-show")) {
 				popup.classList.remove("modal-error");
 			}
-						
+			
+			if (popup.classList.contains("modal-show")) {
+				dateOfArrival.focus(); /* при открытии формы фокус автоматически устанавливался в поле ввода даты заезда в отель */
+			}
+			
 			if (storage) {
 				adults.value = storageAdults;
 				children.value = storageChildren;
 			}
 			
-			dateOfArrival.focus(); /* при открытии формы фокус автоматически устанавливался в поле ввода даты заезда в отель */
+			
 		});
 		
 		form.addEventListener("submit", function (evt) { /* Отловим событие отправки формы и отменим его, если какое-то из полей незаполнено. */

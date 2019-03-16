46533fa7a11df53b86409cd9e9dc08650669812d
SoundM <galina45192@gmail.com>
2019-03-16 19:22:56 +0100

    Защита. Исправление замечаний

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
-		var link = document.querySelector(".button-hotel-search"); /* Находим кнопку, клик по которой покажет модальное окно */
+		var link = document.querySelector(".button-hotel-search"); 
 		
-		var popup = document.querySelector(".modal-hotel-search-form"); /*Находим модальное окно в разметке и записываем его в переменную. Добавляем в стили класс, который будет отвечать за показ модального окна. .modal-show {display: block;} */
+		var popup = document.querySelector(".modal-hotel-search-form"); 
 		
 		var form = popup.querySelector("form");
 		var dateOfArrival = popup.querySelector("[name=date-of-arrival-at-the-hotel]");
@@ -8,7 +8,7 @@
 		var adults = popup.querySelector("[name=number-of-adults]");
 		var children = popup.querySelector("[name=number-of-children]");
 		
-		var isStorageSupport = true; /* Некоторые браузеры не имеют поддержку localStorage, или он может быть отключен, нам стоит учитывать такую возможность. */
+		var isStorageSupport = true; 
 		var storageAdults = "";
 		var storageChildren = "";
 		
@@ -22,16 +22,16 @@
 		}
 		
 		
-		link.addEventListener("click", function (evt) { /* Поймаем событие клика по этой кнопке. */
-			evt.preventDefault(); /*Отменим стандартное действие ссылки при нажатии на неё. */
-			popup.classList.toggle("modal-show"); /* С помощью метода classList.toggle добавляем этот класс к модальному окну по клику на ссылку, а при повторном клике убираем класс. т.е. он поочередно то добавляется, то исчезает */
+		link.addEventListener("click", function (evt) { 
+			evt.preventDefault(); 
+			popup.classList.toggle("modal-show"); 
 			
 			if (!popup.classList.contains("modal-show")) {
 				popup.classList.remove("modal-error");
 			}
 			
 			if (popup.classList.contains("modal-show")) {
-				dateOfArrival.focus(); /* при открытии формы фокус автоматически устанавливался в поле ввода даты заезда в отель */
+				dateOfArrival.focus(); 
 			}
 			
 			if (storage) {
@@ -42,7 +42,7 @@
 			
 		});
 		
-		form.addEventListener("submit", function (evt) { /* Отловим событие отправки формы и отменим его, если какое-то из полей незаполнено. */
+		form.addEventListener("submit", function (evt) { 
 			if (!dateOfArrival.value || !dateOfDeparture.value || !adults.value || !children.value) {
 				evt.preventDefault();
 				popup.classList.remove("modal-error");
